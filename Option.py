fe80b562e60f1730f8d73b2752d6d3d3b0a998b3
Corey Pritchard <corey.pritchard2003@gmail.com>
2025-06-21 16:41:08 +0100

    remove openInterest <= 0

--- Option.py
+++ Option.py
@@ -16,3 +16,20 @@ class Put:
         self.ticker = ticker
         self.yf_ticker = yf.Ticker(ticker)
         self.df = pd.DataFrame()
+
+def get_yf_options(ticker):
+    """
+    Collects all calls and puts with openInterest for a given ticker.
+    """
+
+    yf_ticker = yf.Ticker(ticker)
+    options = yf_ticker.options
+    calls = []
+    puts = []
+
+    for expiry in options:
+        opt_chain = yf_ticker.option_chain(expiry)
+        calls.extend(opt_chain.calls[opt_chain.calls['openInterest'] > 0].to_dict('records'))
+        puts.extend(opt_chain.puts[opt_chain.puts['openInterest'] > 0].to_dict('records'))
+
+    return calls, puts
